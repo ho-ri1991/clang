ff76a416767fd738b281ff1420424348943bf58e
ho-ri1991 <hage.1991@hotmail.co.jp>
2018-10-28 21:24:04 +0900

    remove debug code

--- lib/Parse/ExtendParser.cpp
+++ lib/Parse/ExtendParser.cpp
@@ -1,6 +1,5 @@
 #include "clang/Parse/ExtendParser.h"
 #include "clang/Lex/Preprocessor.h"
-#include "clang/DebugMessage.h"
 
 using namespace clang;
 
@@ -41,45 +40,34 @@ StmtResult
 ExtendParser::ParseStatementOrDeclaration(StmtVector &Stmts, AllowedConstructsKind Allowed,
                                            SourceLocation *TrailingElseLoc)
 {
-  DebugMessage d("ParseStatementOrDeclaration");
-  PP.DumpToken(Tok);
-  std::cout << std::endl;
   return Parser::ParseStatementOrDeclaration(Stmts, Allowed, TrailingElseLoc);
 }
 
 ExprResult
 ExtendParser::ParseExpression(TypeCastState isTypeCast)
 {
-  DebugMessage d("ParseExpression");
-  PP.DumpToken(Tok);
-  std::cout << std::endl;
   return Parser::ParseExpression(isTypeCast);
 }
 
 bool
 ExtendParser::ParseTopLevelDecl(DeclGroupPtrTy &Result)
 {
-  DebugMessage d("ParseTopLevelDecl");
-  PP.DumpToken(Tok);
-  std::cout << std::endl;
 //  if (Tok.is(tok::identifier) && std::string(Tok.getIdentifierInfo()->getNameStart()) == "Test")
 //    TestII = Tok.getIdentifierInfo();
   if (Tok.is(tok::identifier) && std::string(Tok.getIdentifierInfo()->getNameStart()) == "hoge")
   {
 //    assert(TestII);
-    std::cout<< "identifier hoge!" << std::endl;
     auto loc = Tok.getLocation();
     ConsumeAnyToken();
     CachedTokens toks;
-    toks.push_back(GenerateToken(tok::kw_void));
+    toks.push_back(GenerateToken(tok::kw_void, loc));
     toks.push_back(GenerateIdentifierToken(PP, (new std::string("Test"))->c_str(), loc));
-//    toks.push_back(GenerateIdentifierToken(TestII, loc));
-    toks.push_back(GenerateToken(tok::coloncolon));
+    toks.push_back(GenerateToken(tok::coloncolon, loc));
     toks.push_back(GenerateIdentifierToken(PP, (new std::string("print"))->c_str(), loc));
-    toks.push_back(GenerateToken(tok::l_paren));
-    toks.push_back(GenerateToken(tok::r_paren));
-    toks.push_back(GenerateToken(tok::l_brace));
-    toks.push_back(GenerateToken(tok::r_brace));
+    toks.push_back(GenerateToken(tok::l_paren, loc));
+    toks.push_back(GenerateToken(tok::r_paren, loc));
+    toks.push_back(GenerateToken(tok::l_brace, loc));
+    toks.push_back(GenerateToken(tok::r_brace, loc));
     toks.push_back(Tok);
     PP.EnterTokenStream(toks, true);
     ConsumeAnyToken();
